092a88f4ee0d25150156abef2915efef780e30d7
Unknown <vs.litvinov@gmail.com>
2018-09-04 18:01:55 +0400

    java window

--- js/script.js
+++ js/script.js
@@ -3,28 +3,51 @@ var popup = document.querySelector(".modal");
 var close = popup.querySelector(".modal-close");
 var lfname = popup.querySelector("[name=last-first-name]");
 var form = popup.querySelector(".appointment-form");
-var email = popup.querySelector("[name=email-adress]");
-var postmail = popup.querySelector("[name=mail]");
+var postmail = popup.querySelector("[name=email-adress]");
+var textarea = popup.querySelector("[name=mail]");
+var isStorageSupport = true;
+var storage = "";
 
+  try {
+    storage = localStorage.getItem("lfname");
+  } catch (err) {
+    isStorageSupport = false;
+  }
 
 link.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("modal-show");
+    if (storage) {
+      lfname.value = storage;
+      postmail.focus();
+    } else {
     lfname.focus();
+  }
   });
 
 close.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.remove("modal-show");
+  
+    popup.classList.remove("modal-error");
   });
 
 form.addEventListener("submit", function (evt) {
-    evt.preventDefault();
+    if (!lfname.value || !postmail.value || !textarea.value) {
+      evt.preventDefault();
+      popup.classList.add("modal-error");
+    }else {if (isStorageSupport) {
+      localStorage.setItem("lfname", lfname.value  );
+    }
+          }
   });
 
-form.addEventListener("submit", function (evt) {
-        if (!lfname.value || !email-adress.value || !postmail.value) {
-          evt.preventDefault();
-          console.log("Нужно ввести логин и пароль");
-        }
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal-show")) {
+        popup.classList.remove("modal-show");
+        popup.classList.remove("modal-error");
+      }
+    }
   });
